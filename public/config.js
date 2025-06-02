67fff53e4e38bb1574ab882795e17df6763fdd06
oladiran ayanfe <oladiranayanfe7@gmail.com>
2025-06-02 10:52:52 +0100

    refactor: clean up unused code and comments in dashboard.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBxYAPAOqWxB3G4Rb1yEtoOJ3ep6hSFLEw",
-  authDomain: "project-6799379456904993133.firebaseapp.com",
-  databaseURL: "https://project-6799379456904993133-default-rtdb.firebaseio.com",
-  projectId: "project-6799379456904993133",
-  storageBucket: "project-6799379456904993133.firebasestorage.app",
-  messagingSenderId: "255403104368",
-  appId: "1:255403104368:web:7946a3a93d59606a88fcb5",
-};
+export   const firebaseConfig = {
+    apiKey: "AIzaSyBxYAPAOqWxB3G4Rb1yEtoOJ3ep6hSFLEw",
+    authDomain: "project-6799379456904993133.firebaseapp.com",
+    databaseURL: "https://project-6799379456904993133-default-rtdb.firebaseio.com",
+    projectId: "project-6799379456904993133",
+    storageBucket: "project-6799379456904993133.firebasestorage.app",
+    messagingSenderId: "255403104368",
+    appId: "1:255403104368:web:7946a3a93d59606a88fcb5"
+  };
--- public/dashboard.js
+++ public/dashboard.js
@@ -22,7 +22,6 @@ onAuthStateChanged(auth, (user) => {
         }, 500)
     }
 });
-
 const addTodo = () => {
     const myTodo = document.getElementById('todo').value
     if (myTodo == '') {
@@ -33,79 +32,58 @@ const addTodo = () => {
         let date = new Date().toLocaleDateString()
         const todoObj = { myTodo, time, date }
         console.log(todoObj);
-        const todoRef = ref(database, 'todos/2')
-        set(todoRef, todoObj) 
+        // Generate a unique key for each todo
+        const newTodoRef = ref(database, 'todos/' + Date.now());
+        set(newTodoRef, todoObj)
     }
 };
+
 let newRef = ref(database, 'todos')
+
 onValue(newRef, (snapshot) => {
     const data = snapshot.val();
     console.log(data);
     display.innerHTML = ''
-    data.map ((info, i)=>{
-        display.innerHTML += `
-            <p>${i+1}. <strong>${info.myTodo}</strong></p>
-            <small>${info.time} ${info.date}</small>
-        `
-    })
+    if (data) {
+        Object.entries(data).forEach(([key, info], i) => {
+            display.innerHTML += `
+            <div id="todo-${key}">
+                <p>${i + 1}. <strong>${info.myTodo}</strong></p>
+                <small>${info.time} ${info.date}</small>
+                <button onclick="editTodo('${key}', '${info.myTodo.replace(/'/g, "\\'")}')">Edit</button>
+                <button onclick="deleteTodo('${key}')">Delete</button>
+            </div>
+            `;
+        });
+
+        // Edit todo function
+        window.editTodo = (key, currentValue) => {
+            const newValue = prompt("Edit your todo:", currentValue);
+            if (newValue !== null && newValue.trim() !== "") {
+            const todoRef = ref(database, 'todos/' + key);
+            set(todoRef, {
+                myTodo: newValue,
+                time: new Date().toLocaleTimeString(),
+                date: new Date().toLocaleDateString()
+            });
+            }
+        };
+
+        // Delete todo function
+        window.deleteTodo = (key) => {
+            if (confirm("Are you sure you want to delete this todo?")) {
+            const todoRef = ref(database, 'todos/' + key);
+            set(todoRef, null);
+            }
+        };
+        }
+        // Clear input after adding todo
+        window.addTodo = () => {
+            addTodo();
+            document.getElementById('todo').value = '';
+        };
 });
 window.addTodo = addTodo;
-
-
-
-
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-// import { firebaseConfig } from "./config.js";
-// import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
-// import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-database.js";
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app)
-// const database = getDatabase(app)
-
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//         console.log(user);
-//         show.innerHTML = `
-//             <h3>Welcome ${user.displayName}</h3>
-//             <img src=${user.photoURL} alt="Profile Picture" style="width: 100px; height: 100px; border-radius: 100%;" />
-//         `
-//     } else {
-//         setTimeout(() => {
-//             window.location.href = 'signin.html'
-//         }, 500)
-//     }
-// });
-
-// const addTodo = () => {
-//     const myTodo = document.getElementById('todo').value
-//     if (myTodo == '') {
-//         alert('emptyyyyyyyyyyyyyyyyyyyyyy!!')
-//     } else {
-//         console.log(myTodo);
-//         let time = new Date().toLocaleTimeString()
-//         let date = new Date().toLocaleDateString()
-//         const todoObj = { myTodo, time, date }
-//         console.log(todoObj);
-//         const todoRef = ref(database, 'todos/5')
-//         set(todoRef, todoObj)
-//     }
-// }
-
-// let newRef = ref(database, 'todos')
-// onValue(newRef, (snapshot) => {
-//     const data = snapshot.val();
-//     console.log(data);
-//     display.innerHTML = ''
-//     data.map((info, i)=>{
-//         display.innerHTML += `
-//             <p>${i+1}. <strong>${info.myTodo}</strong></p>
-//             <small>${info.time} ${info.date}</small>
-//         `
-//     })
-// });
-// window.addTodo = addTodo
-
 
 
         //         // Generate a unique key for each todo
